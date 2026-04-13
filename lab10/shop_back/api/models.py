c63d7053deff0a8b8c9401f010d8a776955635a5
Yerassyl <ibragimoverasyl546@gmail.com>
2026-04-14 00:11:27 +0500

    e

---
+++ lab10/shop_back/api/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    count = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
---
+++ lab10/shop_back/api/views/__init__.py
@@ -0,0 +1,5 @@
+# Раскомментируй нужный уровень для теста. По заданию должен быть Generics:
+from .generics import (
+    ProductListAPIView, ProductDetailAPIView,
+    CategoryListAPIView, CategoryDetailAPIView, CategoryProductsAPIView
+)
